38776f93bdaaffdf6523bc92a2594f6240fff593
rachelschulmann <054rachel054@gmail.com>
2023-01-09 11:13:28 +0200

    day2

---
+++ modulls/day2/3,2.js
@@ -0,0 +1,29 @@
+
+// From function declarations to explicit and implicit
+return functions (one of each).
+function welcome() {
+let welcome = 'Welcome to Appleseeds Bootcamp!';
+return welcome;
+}
+const welcome = (one of each) => {
+    let welcome = 'Welcome to Appleseeds Bootcamp!';
+    return welcome;}
+
+   
+function power(a) {
+let myNumber = a;
+let result = Math.pow(myNumber, 2);
+return result;
+}
+
+// From function expressions to IIFE functions.
+// const squareRoot = a => Math.sqrt(a);
+const squareRoot = (a)=>{
+   return Math.sqrt(a)
+}
+
+// const randomNumbers = (a, b) => Math.random() * (a - b) +
+// b;
+const randomNumbers = (a, b) =>{
+ return Math.random() * (a - b) +
+b;}
---
+++ modulls/day2/3,3.js
@@ -0,0 +1,16 @@
+function percentAgeOfWorld1(a) {
+  let population = 7900000000;
+  const perecent = Math.round((a / population) * 100);
+  const result = perecent.toFixed(2);
+  return result;
+}
+percentAgeOfWorld1(150000000);
+percentAgeOfWorld1(1100000000);
+percentAgeOfWorld1(1200000000);
+
+function percentAgeOfWorld2(a) {
+  let population = 7900000000;
+  const perecent = Math.round((a / population) * 100);
+  const result = perecent.toFixed(2);
+  return result;
+}
